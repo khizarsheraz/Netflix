01173eefe412b3920cd844d7a5e1a0b06db4fa6a
khizarsheraz <khizarsheraz1993@gmail.com>
2024-05-11 18:12:47 +0300

    adding new owasp zap 2 docker image

--- pipeline.groovy
+++ pipeline.groovy
@@ -19,14 +19,14 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/khizarsheraz/Netflix.git'
             }
         }
-        // stage("Sonarqube Analysis "){
-        //     steps{
-        //         withSonarQubeEnv('sonar-server') {
-        //             sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix \
-        //             -Dsonar.projectKey=Netflix ''' 
-        //         }
-        //     }
-        // }
+        stage("Sonarqube Analysis "){
+            steps{
+                withSonarQubeEnv('sonar-server') {
+                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix \
+                    -Dsonar.projectKey=Netflix ''' 
+                }
+            }
+        }
 
         // stage("quality gate"){
         //    steps {
@@ -37,90 +37,90 @@ pipeline{
         // }
 
 // i've commented quality gate it because on my machine it was taking too much time.
-        // stage('Install Dependencies') {
-        //     steps {
-        //         sh "npm install"
-        //     }
-        // }
-        // stage('OWASP  Dependency Check FS SCAN') {
-        //     steps {
-        //         dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
-        //         dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-        //     }
-        // }
-        // stage('TRIVY FS SCAN - Vulnerability Scan - Docker File') {
-        //     steps {
-        //         sh "trivy fs . > trivyfs.txt"
-        //     }
-        // }
+        stage('Install Dependencies') {
+            steps {
+                sh "npm install"
+            }
+        }
+        stage('OWASP  Dependency Check FS SCAN') {
+            steps {
+                dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
+                dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+            }
+        }
+        stage('TRIVY FS SCAN - Vulnerability Scan - Docker File') {
+            steps {
+                sh "trivy fs . > trivyfs.txt"
+            }
+        }
 
-        // stage('OPA Conftest - Docker File Scan'){
-        //     steps {
-        //         catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-        //         sh 'sudo conftest test --policy /root/Netflix/Opa-Docker-Security.rego /root/Netflix/Dockerfile'
+        stage('OPA Conftest - Docker File Scan'){
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                sh 'sudo conftest test --policy /root/Netflix/Opa-Docker-Security.rego /root/Netflix/Dockerfile'
                 
-        //         }
-        //     }
-        // }   
-        // stage("Docker Build & Push"){
-        //     steps{
-        //         script{
-        //             withDockerRegistry(credentialsId: 'docker', toolName: 'docker'){   
-        //                sh "sudo docker build --build-arg TMDB_V3_API_KEY=4deef21ee71793e3884296430a8e9a90 -t netflix ."
-        //                sh "sudo docker tag netflix khizarsheraz/netflix:latest "
-        //                sh "sudo docker push khizarsheraz/netflix:latest "
-        //             }
-        //         }
-        //     }
-        // }
-        // stage("TRIVY Docker Image Scan"){
-        //     steps{
-        //         sh "trivy image khizarsheraz/netflix:latest > trivyimage.txt" 
-        //     }
-        // }
-        // stage('Deploy to container'){
-        //     steps{
-        //         sh 'docker run -d -p 8081:80 khizarsheraz/netflix:latest'
-        //     }
-        // }
+                }
+            }
+        }   
+        stage("Docker Build & Push"){
+            steps{
+                script{
+                    withDockerRegistry(credentialsId: 'docker', toolName: 'docker'){   
+                       sh "sudo docker build --build-arg TMDB_V3_API_KEY=4deef21ee71793e3884296430a8e9a90 -t netflix ."
+                       sh "sudo docker tag netflix khizarsheraz/netflix:latest "
+                       sh "sudo docker push khizarsheraz/netflix:latest "
+                    }
+                }
+            }
+        }
+        stage("TRIVY Docker Image Scan"){
+            steps{
+                sh "trivy image khizarsheraz/netflix:latest > trivyimage.txt" 
+            }
+        }
+        stage('Deploy to container'){
+            steps{
+                sh 'docker run -d -p 8081:80 khizarsheraz/netflix:latest'
+            }
+        }
 
         
-        // stage('OPA Conftest - Kubernetes File Scan'){
-        //     steps {
-        //         catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-        //         sh 'sudo conftest test --policy /root/Netflix/opa-k8s-security.rego /root/Netflix/Kubernetes/deployment.yml'
-        //         }
-        //     }
-        // } 
+        stage('OPA Conftest - Kubernetes File Scan'){
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                sh 'sudo conftest test --policy /root/Netflix/opa-k8s-security.rego /root/Netflix/Kubernetes/deployment.yml'
+                }
+            }
+        } 
 
-    //    stage('KubeSec - Kubernetes Vulnerability scan'){
-    //         steps {
-    //             catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-    //             sh 'sudo kubesec scan /root/Netflix/Kubernetes/deployment.yml'
-    //             } 
-    //         }
-    //     } 
+       stage('KubeSec - Kubernetes Vulnerability scan'){
+            steps {
+                catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                sh 'sudo kubesec scan /root/Netflix/Kubernetes/deployment.yml'
+                } 
+            }
+        } 
 
 
-        // stage('Deploy to kubernets'){
-        //     steps{
-        //         script{
-        //             dir('Kubernetes') {
-        //                 withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'kubeconfig', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
-        //                         sh 'kubectl apply -f deployment.yml'
-        //                         sh 'kubectl apply -f service.yml'
-        //                 }   
-        //             }
-        //         }
-        //     }
-        // }
+        stage('Deploy to kubernets'){
+            steps{
+                script{
+                    dir('Kubernetes') {
+                        withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'kubeconfig', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
+                                sh 'kubectl apply -f deployment.yml'
+                                sh 'kubectl apply -f service.yml'
+                        }   
+                    }
+                }
+            }
+        }
 
 
         
          stage('Owasp ZAP - DAST') {
                 
                     environment {
-                        ZAP_DOCKER_IMAGE = ' owasp/zap2docker-weekly'
+                        ZAP_DOCKER_IMAGE = 'ictu/zap2docker-weekly' //this is the image id of owasp/zap2docker-stable, as my linux version is amd64, i'm specifying the already downloaded image
                         ZAP_TARGET = 'http://192.168.100.148:30007'
                         DOCKER_PLATFORM = 'linux/amd64'
                     }
@@ -137,10 +137,10 @@ pipeline{
                                         // Continue pipeline execution even if OWASP ZAP scan fails
                                             }
                             //below command will remove all the containers build upon owasp image
-                            // sh "docker ps -a | awk '\$2 ~ /owasp\\/zap2docker-weekly/ {print \$1}' | xargs docker rm" 
-                            // sh "echo owasp images removed"               
-                            // sh "mkdir -p owasp-zap-report"
-                            // sh "sudo mv /var/lib/jenkins/zap_report2.html owasp-zap-report"
+                            sh "docker ps -a | awk '\$2 ~ /owasp\\/zap2docker-weekly/ {print \$1}' | xargs docker rm" 
+                            sh "echo owasp images removed"               
+                            sh "mkdir -p owasp-zap-report"
+                            sh "sudo mv /var/lib/jenkins/zap_report2.html owasp-zap-report"
                     }
                 }
                        post {
